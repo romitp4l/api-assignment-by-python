fdc332a3929973b3b6e2f1ff433246a81253c8f2
ROMIT PAL <85341342+romitp4l@users.noreply.github.com>
2025-04-20 02:05:49 +0530

    Update api.py

--- api.py
+++ api.py
@@ -139,5 +139,5 @@ def calculate_cost():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
